a55fc1aebbb07c55da3a081e7ffcaca6fc108c64
Isaac Kang <ikang005@wch136-09.cs.ucr.edu>
2020-04-11 14:32:05 -0700

    Won't compile

--- Lab3_BitManip/source/main.c
+++ Lab3_BitManip/source/main.c
@@ -13,37 +13,42 @@
 #endif
 
 int main(void) {
-        DDRA = 0x00; PORTA = 0xFF;
-        DDRB = 0x00; PORTB = 0xFF;
-        DDRC = 0xFF; PORTC = 0x00;
-
-        unsigned char numA = 0x00;
-        unsigned char numB = 0x00;
-        unsigned char totA = 0x00;
-        unsigned char totB = 0x00;
-        unsigned char i = 0x00;
-        unsigned char tmp = 0x00;
-        unsigned char tmp2 = 0x00;
-
-        while (1) {
-                numA = PINA;
-                numB = PINB;
-                totA = 0x00;
-                totB = 0x00;
-
-                while(i <= 0x08){
-                        tmp = (numA >> i) & 0x01;
-                        tmp2 = (numB >> i) & 0x01;
-                        if(tmp == 0x01){
-                                totA = totA + 0x01;
-                        }
-                        if(tmp2 == 0x01){
-                                totB = totB + 0x01;
-                        }
-                }
-                PORTC = totA + totB;
-                i = 0x00;
-        }
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	
+	unsigned char numA = 0x00;
+	unsigned char numB = 0x00;
+	unsigned char totA = 0x00;
+	unsigned char totB = 0x00;
+	unsigned char i = 0;
+	unsigned char tmp = 0x00;
+	
+    while(1)
+    {
+		numA = PINA;
+		numB = PINB;
+		totA = 0x00;
+		totB = 0x00;
+		for(i; i < 0x08; i = i + 0x01)
+		{
+			tmp = (numA >> i) & 0x01;
+			if(tmp == 0x01)
+			{
+				totA = totA + 0x01;
+			}
+		}
+		i = 0x00;
+		for(i; i < 0x08; i = i + 0x01)
+		{
+			tmp = (numB >> i) & 0x01;
+			if(tmp == 0x01)
+			{
+				totB = totB + 0x01;
+			}
+		}
+		PORTC = totA + totB;	
+    }
 
 
     return 0;
