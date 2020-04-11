18d87aa297c555440e00369e2a7676230a277e6c
Isaac Kang <ikang005@wch136-09.cs.ucr.edu>
2020-04-11 13:40:02 -0700

    Lab3 #1

---
+++ Lab3_BitManip/source/main.c
@@ -0,0 +1,50 @@
+/*	Author: ikang005
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+        DDRA = 0x00; PORTA = 0xFF;
+        DDRB = 0x00; PORTB = 0xFF;
+        DDRC = 0xFF; PORTC = 0x00;
+
+        unsigned char numA = 0x00;
+        unsigned char numB = 0x00;
+        unsigned char totA = 0x00;
+        unsigned char totB = 0x00;
+        unsigned char i = 0x00;
+        unsigned char tmp = 0x00;
+        unsigned char tmp2 = 0x00;
+
+        while (1) {
+                numA = PINA;
+                numB = PINB;
+                totA = 0x00;
+                totB = 0x00;
+
+                while(i <= 0x08){
+                        tmp = (numA >> i) & 0x01;
+                        tmp2 = (numB >> i) & 0x01;
+                        if(tmp == 0x01){
+                                totA = totA + 0x01;
+                        }
+                        if(tmp2 == 0x01){
+                                totB = totB + 0x01;
+                        }
+                }
+                PORTC = totA + totB;
+                i = 0x00;
+        }
+
+
+    return 0;
+}
