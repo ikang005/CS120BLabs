bd5e615f9004dd54a0286f921dec70eec7125d53
ikang005 <62870107+ikang005@users.noreply.github.com>
2020-04-12 14:48:42 -0700

    change

--- Lab3_BitManip/turnin/ikang005_lab3_part2.c
+++ Lab3_BitManip/turnin/ikang005_lab3_part2.c
@@ -1,8 +1,9 @@
 /*	Author: ikang005
- *	Partner(s) Name: 
+ *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #3  Exercise #2
+ *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
+ *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
@@ -16,64 +17,62 @@ int main(void) {
     DDRC = 0xFF; PORTC = 0x00;
     
     unsigned char fl = 0x00;
-    unsigned char sensor = 0x00;
     
     while(1){
-        if((fl & 0x00) == 0x00){
-            sensor = 0x40;
+        fl = PINA;
+        switch(fl){
+            case 0x00:
+                PORTC = 0x40;
+                break;
+            case 0x01:
+                PORTC = 0x60;
+                break;
+            case 0x02:
+                PORTC = 0x60;
+                break;
+            case 0x03:
+                PORTC = 0x70;
+                break;
+            case 0x04:
+                PORTC = 0x70;
+                break;
+            case 0x05:
+                PORTC = 0x38;
+                break;
+            case 0x06:
+                PORTC = 0x38;
+                break;
+            case 0x07:
+                PORTC = 0x3C;
+                break;
+            case 0x08:
+                PORTC = 0x3C;
+                break;
+            case 0x09:
+                PORTC = 0x3C;
+                break;
+            case 0x0A:
+                PORTC = 0x3E;
+                break;
+            case 0x0B:
+                PORTC = 0x3E;
+                break;
+            case 0x0C:
+                PORTC = 0x3E;
+                break;
+            case 0x0D:
+                PORTC = 0x3F;
+                break;
+            case 0x0E:
+                PORTC = 0x3F;
+                break;
+            case 0x0F:
+                PORTC = 0x3F;
+                break;
+            default:
+                break;
         }
-        if((fl & 0x01) == 0x01){
-            sensor = 0x60;
-        }
-        if((fl & 0x02) == 0x02){
-            sensor = 0x60;
-        }
-        if((fl & 0x03) == 0x03){
-            sensor = 0x70;
-        }
-        if((fl & 0x04) == 0x04){
-            sensor = 0x70;
-        }
-        if((fl & 0x05) == 0x05){
-            sensor = 0x38;
-        }
-        if((fl & 0x06) == 0x06){
-            sensor = 0x38;
-        }
-        if((fl & 0x07) == 0x07){
-            sensor = 0x3C;
-        }
-        if((fl & 0x08) == 0x08){
-            sensor = 0x3C;
-        }
-        if((fl & 0x09) == 0x09){
-            sensor = 0x3C;
-        }
-        if((fl & 0x0A) == 0x0A){
-            sensor = 0x3E;
-        }
-        if((fl & 0x0B) == 0x0B){
-            sensor = 0x3E;
-        }
-        if((fl & 0x0C) == 0x0C){
-            sensor = 0x3E;
-        }
-        if((fl & 0x0D) == 0x0D){
-            sensor = 0x3F;
-        }
-        if((fl & 0x0E) == 0x0E){
-            sensor = 0x3F;
-        }
-        if((fl & 0x0F) == 0x0F){
-            sensor = 0x3F;
-        }
-        PORTC = sensor;
-        fl = 0x00;
-        sensor = 0x00;
     }
-
-
     return 0;
 }
-
 
