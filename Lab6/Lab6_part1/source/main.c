b213ef49c0ca5aea6f138ac95d8950495b15e6eb
Isaac Kang <ikang005@wch136-09.cs.ucr.edu>
2020-05-05 12:57:23 -0700

    Started Lab7

--- Lab6/Lab6_part1/source/main.c
+++ Lab6/Lab6_part1/source/main.c
@@ -14,45 +14,10 @@
 #include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "timer.h"
 #endif
 
-volatile unsigned char TimerFlag = 0;
-
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
-unsigned char tmp = 0x00;
-
-enum States{Start, First, Second, Third}state;
-
-void TimerOn(){
-    TCCR1B = 0x0B;
-    OCR1A = 125;
-    TIMSK1 = 0x02;
-    TCNT1 = 0;
-    _avr_timer_cntcurr = _avr_timer_M;
-    SREG |= 0x80;
-}
-
-void TimerOff(){
-    TCCR1B = 0x00;
-}
-
-void TimerISR(){
-    TimerFlag = 1;
-}
-
-ISR(TIMER1_COMPA_vect){
-    _avr_timer_cntcurr--;
-    if(_avr_timer_cntcurr == 0){
-        TimerISR();
-        _avr_timer_cntcurr = _avr_timer_M;
-    }
-}
-
-void TimerSet(unsigned long M){
-    _avr_timer_M = M;
-    _avr_timer_cntcurr = _avr_timer_M;
-}
+enum States{Start, First, Second, Third}states;
 
 void Tick(){
     switch(state){
---
+++ Lab7/Lab7_part1/source/main.c
@@ -0,0 +1,197 @@
+/*	Author: ikang005
+ *      Partner(s) Name: 
+ *	Lab Section: 21
+ *	Assignment: Lab #7  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *	INC & DEC with LED
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "io.h"
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States{Start, Init, Inc, Dec, Wait, Reset} state;
+unsigned char button;
+unsigned char tmp;
+unsigned char i;
+
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn(){
+    TCCR1B = 0x0B;
+    OCR1A = 125;
+    TIMSK1 = 0x02;
+    TCNT1 = 0;
+    _avr_timer_cntcurr = _avr_timer_M;
+    SREG |= 0x80;
+}
+
+void TimerOff(){
+    TCCR1B = 0x00;
+}
+
+void TimerISR(){
+    TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect){
+    _avr_timer_cntcurr--;
+    if(_avr_timer_cntcurr == 0) {
+        TimerISR();
+        _avr_timer_cntcurr = _avr_timer_M;
+    }
+}
+
+void TimerSet(unsigned long M){
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
+}
+
+
+void Tick(){
+    switch(state){
+        case Start:
+            state = Init;
+            break;
+        case Init:
+            if(button == 0x01){
+                state = Inc;
+                if(tmp < 0x09){
+                    tmp += 0x01;
+                }
+            }
+            else if(button == 0x02){
+                state = Dec;
+                if(tmp > 0){
+                    tmp -= 0x01;
+                }
+            }
+            else if(button == 0x03){
+                state = Reset;
+            }
+            else{
+                state = Init;
+            }
+            break;
+        case Inc:
+            if(button == 0x00){
+                state = Wait;
+            }
+            else if(button == 0x03){
+                state = Reset;
+            }
+            else{
+                state = Inc;
+            }
+            break;
+        case Dec:
+            if(button == 0x00){
+                state = Wait;
+            }
+            else if(button == 0x03){
+                state = Reset;
+            }
+            else{
+                state = Dec;
+            }
+            break;
+        case Wait:
+            if(button == 0x01){
+                state = Inc;
+                if(tmp < 0x09){
+                    tmp += 0x01;
+                }
+            }
+            else if(button == 0x02){
+                state = Dec;
+                if(tmp > 0x00){
+                    tmp -= 0x01;
+                }
+            }
+            else if(button == 0x03){
+                state = Reset;
+            }
+            else{
+                state = Wait;
+            }
+            break;
+        case Reset:
+            if(button == 0x03){
+                state = Reset;
+            }
+            else{
+                state = Wait;
+            }
+            break;
+        default:
+            state = Start;
+            break;
+    }
+    switch(state){
+        case Start:
+            tmp = 0x07;
+            break;
+        case Init:
+            tmp = 0x07;
+            i = 0;
+            break;
+        case Inc:
+            if(tmp < 0x09 && i == 10){
+                tmp += 0x01;
+                i = 0;
+            }
+            i++;
+            break;
+        case Dec:
+            if(tmp > 0x00 && i == 10){
+                tmp -= 0x01;
+                i = 0;
+            }
+            i++;
+            break;
+        case Wait:
+            i = 0;
+            break;
+        case Reset:
+            tmp = 0x00;
+            i = 0;
+            break;
+        default:
+            tmp = 0x07;
+            i = 0;
+            break;
+    }
+}
+
+int main(void)
+{
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xFF; PORTC = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
+
+    LCD_init();
+    TimerSet(100);
+    TimerOn();
+    tmp = 0x07;
+    state = start;
+    
+    while(1){
+        button = ~PINA & 0x03;
+        Tick();
+        while(!TimerFlag);
+        TimerFlag = 0;
+        PORTB = tmp;
+	LCD_Cursor(1);
+	LCD_WriteData(tmp + '0');
+    }
+    return 0;
+}
+
